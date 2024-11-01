7e25a83cf8fce33860b2aa1d3b85c365405de42c
ITVash <49442123+ITVash@users.noreply.github.com>
2024-11-01 18:47:21 +0300

    Fixed Styles

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -16,7 +16,7 @@ import { useTelegram } from "@/shared/lib/providers"
 export default function Home() {
 	const { user, webApp } = useTelegram()
 	return (
-		<div className='flex flex-col max-w-sm min-h-svh'>
+		<div className='flex flex-row max-w-sm min-h-[100vh]'>
 			<div className='flex mt-4 max-w-sm'>
 				{user ? (
 					<>
