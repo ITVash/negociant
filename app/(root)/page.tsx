08674f1f9ad60a64dbbbafa8732b8faa86906fb9
ITVash <49442123+ITVash@users.noreply.github.com>
2024-11-01 18:25:44 +0300

    Fixed Styles

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
 	const { user, webApp } = useTelegram()
 	return (
 		<div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-			<div>
+			<div className='flex mt-4 max-w-sm'>
 				{user ? (
 					<>
 						<h1>Welcome {user?.username}</h1>
