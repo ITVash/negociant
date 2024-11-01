a12bf93d76f1ff291a9379ee7bec5ade580e8cb6
ITVash <49442123+ITVash@users.noreply.github.com>
2024-11-01 21:13:37 +0300

    Fixed Dark Theme-6

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,7 @@ const config: Config = {
 				"telegram-primary": "var(--telegram-button-color)",
 				"telegram-primary-text": "var(--telegram-button-text-color)",
 				"telegram-secondary-white": "var(--telegram-secondary-bg-color)",
+				"telegram-color-text": "var(--color-text)",
 				background: "hsl(var(--background))",
 				foreground: "hsl(var(--foreground))",
 				card: {
