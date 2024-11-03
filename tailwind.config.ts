2f7effe40729d486f8c86230f3718b46ba5e0ad8
ITVash <49442123+ITVash@users.noreply.github.com>
2024-11-03 13:34:48 +0300

    delit telegram color theme-1

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,7 @@ import type { Config } from "tailwindcss"
  */
 
 const config: Config = {
-	darkMode: ["class"],
+	//darkMode: ["class"],
 	content: [
 		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
 		"./shared/components/**/*.{js,ts,jsx,tsx,mdx}",
