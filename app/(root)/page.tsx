7a4c885c620f3122c7037a6ab626e4d3f3e4df46
ITVash <49442123+ITVash@users.noreply.github.com>
2024-11-04 12:26:58 +0300

    Begining design-2

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -14,21 +14,23 @@ export default function Home() {
 		}
 	}, [])
 	return (
-		<Container className={`text-[${colors?.text_color}]`}>
-			{user ? (
-				<>
-					<h1>
-						Добро пожаловать: {user!.first_name} {user!.last_name}
-					</h1>
-					<p>
-						Ваша цветовая схема: <pre>{JSON.stringify(colors)}</pre>
-					</p>
-					User data:
-					<pre>{JSON.stringify(user, null, 2)}</pre>
-				</>
-			) : (
-				<p>Приложение необходимо открывать, только в телеграмме </p>
-			)}
+		<Container className={`text-[${colors?.text_color}] flex-row`}>
+			<div>
+				{user ? (
+					<>
+						<h1>
+							Добро пожаловать: {user!.first_name} {user!.last_name}
+						</h1>
+					</>
+				) : (
+					<p>Приложение необходимо открывать, только в телеграмме </p>
+				)}
+			</div>
+			<div>
+				<p>
+					Ваша цветовая схема: <pre>{JSON.stringify(colors)}</pre>
+				</p>
+			</div>
 		</Container>
 	)
 }
