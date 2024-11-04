7567249255313105e04fb048995a6cab10514345
ITVash <49442123+ITVash@users.noreply.github.com>
2024-11-04 18:14:56 +0300

    Add routing todo

--- app/(root)/detail/[id]/page.tsx
+++ app/(root)/detail/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { Container } from "@/shared/components/shared"
+import { useTelegram } from "@/shared/lib/providers"
 import { cn } from "@/shared/lib/utils"
 
 export default async function Detail({
@@ -6,8 +7,13 @@ export default async function Detail({
 }: {
 	params: { id: string }
 }) {
+	const { webApp } = useTelegram()
+	if (!webApp) {
+		return <p>Загрузка...</p>
+	}
 	return (
-		<Container className='text-[#FFFFFF]'>
+		<Container
+			className={`text-[#ffffff] text-[${webApp.themeParams.text_color}]`}>
 			<div className={cn("")}>Detail {id}</div>
 		</Container>
 	)
