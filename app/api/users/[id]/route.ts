626042939269fc2bf03fda393ad3f05e58a9cd45
ITVash <49442123+ITVash@users.noreply.github.com>
2024-11-06 23:03:35 +0300

    created backend user

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -3,11 +3,11 @@ import { NextRequest, NextResponse } from "next/server"
 
 export async function GET(
 	req: NextRequest,
-	{ params }: { params: { tg_id: Number } },
+	{ params }: { params: { tg_id: String } },
 ) {
 	const id = Number(params.tg_id)
 	const data = Number(req.ip)
-	const user = await prisma.negoUser.findFirst({ where: { id_tg: data } })
+	const user = await prisma.negoUser.findFirst({ where: { id_tg: id } })
 	if (!user)
 		return NextResponse.json({
 			message: "Пользователь не был найден в базе данных!",
