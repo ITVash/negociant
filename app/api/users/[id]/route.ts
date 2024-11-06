5b1ac6bc72bb9b20c79c3aff5ad263ef2250764b
ITVash <49442123+ITVash@users.noreply.github.com>
2024-11-06 20:55:55 +0300

    created backend user

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -1,9 +1,9 @@
 import { prisma } from "@/prisma/prisma-client"
 import { NextResponse } from "next/server"
 
-export async function GET({ params }: { params: { tg_id: String } }) {
+export async function GET({ params }: { params: { tg_id: Number } }) {
 	const id = Number(params.tg_id)
-	const user = await prisma.negoUser.findFirst({ where: { id } })
+	const user = await prisma.negoUser.findFirst({ where: { id_tg: id } })
 	if (!user) return false
 	return NextResponse.json(user)
 }
