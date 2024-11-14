1788ec963a6069b43472a4f7c1e0d9fb09039852
ITVash <49442123+ITVash@users.noreply.github.com>
2024-11-14 19:03:44 +0300

    created backend user

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,6 @@
 "use client"
-import { prisma } from "@/prisma/prisma-client"
 import { Container, Header, WorkList } from "@/shared/components/shared"
-import { Button } from "@/shared/components/ui/button"
-import { Checkbox } from "@/shared/components/ui/checkbox"
 import { useTelegram } from "@/shared/lib/providers"
-import { CreateUser } from "@/shared/lib/registerUser"
-import { cn } from "@/shared/lib/utils"
-import { negoUser } from "@prisma/client"
-import { ArrowBigRightDash } from "lucide-react"
-import Link from "next/link"
 import React from "react"
 
 interface ITodo {
@@ -16,12 +8,10 @@ interface ITodo {
 }
 export default function Home() {
 	const { user, webApp } = useTelegram()
-	const [auth, setAuth] = React.useState<negoUser>()
 
 	React.useEffect(() => {
 		if (webApp && user) {
 			webApp!.BackButton.isVisible = false
-			CreateUser(user, setAuth)
 		}
 	}, [])
 	const temple = [
@@ -53,31 +43,6 @@ export default function Home() {
 			className={`text-[#ffffff] text-[${webApp.themeParams.text_color}] flex-col max-h-screen`}>
 			<Header />
 			<WorkList items={temple} />
-			<ul
-				className={cn(
-					"flex h-8 gap-1 bg-[#212121] border-b-sky-900 border-b border-solid",
-					`bg-[${webApp.themeParams.header_bg_color}]`,
-				)}>
-				{auth ? (
-					<>
-						<li>Добро пожаловать:</li>
-						<li>
-							{auth.first_name} {auth.last_name}
-						</li>
-					</>
-				) : (
-					<li>
-						<Button
-							variant={"link"}
-							onClick={() => CreateUser(user!, setAuth)}
-							className={cn(
-								`bg-[${webApp.themeParams.button_color}] text-[${webApp.themeParams.button_text_color}]`,
-							)}>
-							Регистрация...
-						</Button>
-					</li>
-				)}
-			</ul>
 		</Container>
 	)
 }
