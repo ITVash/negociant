a89123798878abfbb6e1f3462b033636c15160e5
ITVash <49442123+ITVash@users.noreply.github.com>
2024-11-21 21:29:46 +0300

    created backend All

--- shared/lib/registerUser.ts
+++ shared/lib/registerUser.ts
@@ -2,23 +2,27 @@ import { prisma } from "@/prisma/prisma-client"
 import { ITelegramUser } from "../@types"
 
 export const CreateUser = async (user: ITelegramUser) => {
-	if (user) {
-		const manyUser = await prisma.negoUser.findFirst({
-			where: { id_tg: Number(user.id) },
-		})
-
-		if (!manyUser) {
-			const data = await prisma.negoUser.create({
-				data: {
-					id_tg: user.id,
-					last_name: user.last_name,
-					first_name: user.first_name,
-					photo_url: user.photo_url!,
-					username: user.username,
-					role: "GUEST",
-				},
+	try {
+		if (user) {
+			const manyUser = await prisma.negoUser.findFirst({
+				where: { id_tg: Number(user.id) },
 			})
-			return data
+
+			if (!manyUser) {
+				const data = await prisma.negoUser.create({
+					data: {
+						id_tg: user.id,
+						last_name: user.last_name,
+						first_name: user.first_name,
+						photo_url: user.photo_url!,
+						username: user.username,
+						role: "GUEST",
+					},
+				})
+				return data
+			}
 		}
+	} catch (error) {
+		console.error(error)
 	}
 }
