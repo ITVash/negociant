5a347f898c2de626775f9f747431a74cb9eba6f3
ITVash <49442123+ITVash@users.noreply.github.com>
2024-12-03 18:13:41 +0300

    Created admin menu

--- shared/components/shared/not-access.tsx
+++ shared/components/shared/not-access.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/shared/lib/utils"
+import { ShieldAlert } from "lucide-react"
 import React from "react"
 
 interface INotAccessProps {
@@ -12,6 +13,7 @@ export const NotAccess: React.FC<INotAccessProps> = ({ className }) => {
 				"flex items-center justify-center min-h-screen text-lg font-bold w-[100%] mx-auto",
 				className,
 			)}>
+			<ShieldAlert />
 			<p className='text-center'>
 				У вас нет доступа к приложению. Обратитесь к администратору!
 			</p>
