175f7b311367ba0d2eddd36eb092062ae5e0fa75
ITVash <49442123+ITVash@users.noreply.github.com>
2024-12-04 17:21:14 +0300

    Created admin menu

--- shared/components/shared/loading.tsx
+++ shared/components/shared/loading.tsx
@@ -1,6 +1,8 @@
 import { cn } from "@/shared/lib/utils"
 import { LoaderCircle } from "lucide-react"
 import React from "react"
+import Logo from "@/public/favicon.svg"
+import Image from "next/image"
 
 interface LoadingProps {
 	className?: string
@@ -13,7 +15,12 @@ export const Loading: React.FC<LoadingProps> = ({ className }) => {
 				"flex flex-col w-[100%] h-screen items-center justify-center m-auto",
 				className,
 			)}>
-			<LoaderCircle className='animate-spin text-sky-800 w-4 h-4' />
+			<Image src={Logo} width={100} height={100} alt='Logo' />
+			<LoaderCircle
+				className='animate-spin text-sky-800 w-4 h-4'
+				width={25}
+				height={25}
+			/>
 		</div>
 	)
 }
