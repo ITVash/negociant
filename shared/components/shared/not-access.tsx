a2826eeb8ba3c78182f0b37e8c5cc75de6b37329
ITVash <49442123+ITVash@users.noreply.github.com>
2024-12-04 17:47:28 +0300

    Created admin menu

--- shared/components/shared/not-access.tsx
+++ shared/components/shared/not-access.tsx
@@ -13,7 +13,7 @@ export const NotAccess: React.FC<INotAccessProps> = ({ className }) => {
 				"flex flex-col items-center justify-center min-h-screen text-lg font-bold w-[100%] mx-auto",
 				className,
 			)}>
-			<ShieldAlert className='h-[100px] w-[100px] text-red-800' />
+			<ShieldAlert className='h-[100px] w-[100px] text-red-800 animate-ping' />
 			<p className='text-center'>
 				У вас нет доступа к приложению. Обратитесь к администратору!
 			</p>
