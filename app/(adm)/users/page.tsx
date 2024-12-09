ecc4e4efde5e9a68928dcb3c1583621c26ee1921
ITVash <49442123+ITVash@users.noreply.github.com>
2024-12-09 21:34:03 +0300

    Created admin menu

--- app/(adm)/users/page.tsx
+++ app/(adm)/users/page.tsx
@@ -1,6 +1,32 @@
+import { Loading } from "@/shared/components/shared"
+import { useTelegram } from "@/shared/lib/providers"
 import { cn } from "@/shared/lib/utils"
+import { useRouter } from "next/navigation"
 import React from "react"
 
 export default function EditUsers() {
-	return <div>EditUsers</div>
+	const { webApp } = useTelegram()
+	const router = useRouter()
+
+	React.useEffect(() => {
+		if (webApp) {
+			webApp.BackButton.isVisible = true
+
+			webApp.onEvent("backButtonClicked", router.back)
+		}
+		return () => {
+			if (webApp) {
+				webApp.BackButton.isVisible = false
+				webApp.offEvent("backButtonClicked", router.back)
+			}
+		}
+	}, [])
+	if (!webApp) {
+		return <Loading />
+	}
+	return (
+		<div className={cn(`text-[${webApp?.themeParams.text_color}]`)}>
+			EditUsers
+		</div>
+	)
 }
--- app/(adm)/works/page.tsx
+++ app/(adm)/works/page.tsx
@@ -1,6 +1,30 @@
+import { Loading } from "@/shared/components/shared"
+import { useTelegram } from "@/shared/lib/providers"
 import { cn } from "@/shared/lib/utils"
+import { useRouter } from "next/navigation"
 import React from "react"
 
 export default function EditWorks() {
-	return <div>Works</div>
+	const { webApp } = useTelegram()
+	const router = useRouter()
+
+	React.useEffect(() => {
+		if (webApp) {
+			webApp.BackButton.isVisible = true
+
+			webApp.onEvent("backButtonClicked", router.back)
+		}
+		return () => {
+			if (webApp) {
+				webApp.BackButton.isVisible = false
+				webApp.offEvent("backButtonClicked", router.back)
+			}
+		}
+	}, [])
+	if (!webApp) {
+		return <Loading />
+	}
+	return (
+		<div className={cn(`text-[${webApp?.themeParams.text_color}]`)}>Works</div>
+	)
 }
