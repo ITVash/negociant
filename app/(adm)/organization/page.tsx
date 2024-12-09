b7bd296820ba21e85a1e97571a2c5821c32223c3
ITVash <49442123+ITVash@users.noreply.github.com>
2024-12-09 21:04:08 +0300

    Created admin menu

--- app/(adm)/organization/page.tsx
+++ app/(adm)/organization/page.tsx
@@ -2,6 +2,6 @@
 import { cn } from "@/shared/lib/utils"
 import React from "react"
 
-export default function EditOrganization({ className }: { className: string }) {
-	return <div className={cn("", className)}>EditOrganization</div>
+export default function EditOrganization() {
+	return <div>EditOrganization</div>
 }
