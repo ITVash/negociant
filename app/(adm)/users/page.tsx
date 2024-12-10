a43b97849a0947f126dbf72afc258d4e337142c7
ITVash <49442123+ITVash@users.noreply.github.com>
2024-12-10 17:03:06 +0300

    Created admin menu

--- app/(adm)/users/page.tsx
+++ app/(adm)/users/page.tsx
@@ -2,12 +2,14 @@
 import { Container, Loading } from "@/shared/components/shared"
 import { useTelegram } from "@/shared/lib/providers"
 import { cn } from "@/shared/lib/utils"
+import { useUser } from "@/shared/store"
 import { useRouter } from "next/navigation"
 import React from "react"
 
 export default function EditUsers() {
 	const { webApp } = useTelegram()
 	const router = useRouter()
+	const { items } = useUser()
 
 	React.useEffect(() => {
 		if (webApp) {
@@ -22,12 +24,26 @@ export default function EditUsers() {
 			}
 		}
 	}, [])
-	if (!webApp) {
+	if (!webApp || !items) {
 		return <Loading />
 	}
 	return (
-		<Container className={cn(`text-[${webApp?.themeParams.text_color}]`)}>
-			EditUsers
+		<Container className={cn(`text-[${webApp?.themeParams.text_color}], mt-4`)}>
+			<h3>Список пользователей!</h3>
+			<ul>
+				<li>
+					<div className='w-[100px]'>Имя</div>
+					<div className='w-[150px]'>Фамилия</div>
+					<div className='w-[80px]'>Роль</div>
+				</li>
+				{items.map((item, id) => (
+					<li key={id} className='flex gap-2'>
+						<div className='w-[100px]'>{item.first_name}</div>
+						<div className='w-[150px]'>{item.last_name}</div>
+						<div className='w-[80px]'>{item.role}</div>
+					</li>
+				))}
+			</ul>
 		</Container>
 	)
 }
